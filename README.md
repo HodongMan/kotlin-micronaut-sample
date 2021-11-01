4a773c4d845a232fb5de617b4499fbb99f3c01cb
hodongman <jhd9206@gmail.com>
2021-11-02 08:14:58 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## Micronaut 3.1.3 Documentation
+
+- [User Guide](https://docs.micronaut.io/3.1.3/guide/index.html)
+- [API Reference](https://docs.micronaut.io/3.1.3/api/index.html)
+- [Configuration Reference](https://docs.micronaut.io/3.1.3/guide/configurationreference.html)
+- [Micronaut Guides](https://guides.micronaut.io/index.html)
+---
+
+## Feature data-r2dbc documentation
+
+- [Micronaut Data R2DBC documentation](https://micronaut-projects.github.io/micronaut-data/latest/guide/#dbc)
+
+- [https://r2dbc.io](https://r2dbc.io)
+
+## Feature r2dbc documentation
+
+- [Micronaut R2DBC documentation](https://micronaut-projects.github.io/micronaut-r2dbc/latest/guide/)
+
+- [https://r2dbc.io](https://r2dbc.io)
+
+## Feature security-jwt documentation
+
+- [Micronaut Security JWT documentation](https://micronaut-projects.github.io/micronaut-security/latest/guide/index.html)
+
+## Feature http-client documentation
+
+- [Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
+
---
+++ build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.5.21"
+    id("groovy") 
+    id("org.jetbrains.kotlin.kapt") version "1.5.21"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("io.micronaut.application") version "2.0.8"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.5.21"
+}
+
+version = "0.1"
+group = "io.asyncevent"
+
+val kotlinVersion=project.properties.get("kotlinVersion")
+repositories {
+    mavenCentral()
+}
+
+micronaut {
+    runtime("netty")
+    testRuntime("spock2")
+    processing {
+        incremental(true)
+        annotations("io.asyncevent.*")
+    }
+}
+
+dependencies {
+    kapt("io.micronaut:micronaut-http-validation")
+    kapt("io.micronaut.data:micronaut-data-processor")
+    kapt("io.micronaut.security:micronaut-security-annotations")
+    implementation("io.micronaut:micronaut-http-client")
+    implementation("io.micronaut:micronaut-runtime")
+    implementation("io.micronaut.data:micronaut-data-r2dbc")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.security:micronaut-security-jwt")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("io.r2dbc:r2dbc-h2")
+    implementation("io.micronaut:micronaut-validation")
+
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+}
+
+
+application {
+    mainClass.set("io.asyncevent.ApplicationKt")
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("1.8")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+
+
+}
